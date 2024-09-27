9a0f42764a7829786e74faa43c1f514270ca32a5
Tom Ritchford <tom@swirly.com>
2024-09-27 19:52:35 +0200

    Extract just the text from fortune.py

--- masbo/bots/fortune.py
+++ masbo/bots/fortune.py
@@ -1,45 +1,10 @@
-from functools import cache
-from mastodon import Mastodon
 import subprocess
-import time
-import traceback
-
-ENABLE_SEND = True
-KWARGS = {'text': True, 'stdout': subprocess.PIPE}
-MAX_LEN = 500
-ACCESS_TOKEN = open('access-token.txt').read().strip()
-BASE_URL = 'https://botsin.space/'
-DELAY = 3601
-TAGS = """
-#humor #unix #usr #games #fortune"""
-MSG_LEN = MAX_LEN - len(TAGS)
 
 
-def mastodon():
-    return Mastodon(
-        access_token = ACCESS_TOKEN,
-        api_base_url = 'https://botsin.space/'
-    )
-
-
-def get_fortune():
-    while len(f := subprocess.run('fortune', **KWARGS).stdout) > MSG_LEN:
-        print('...Too long!', len(f))
-
-    return f
-
-
-def main():
+def fortune(bot):
     while True:
-        f = get_fortune()
-        print('\n' + f + TAGS)
-        try:
-            if ENABLE_SEND:
-                mastodon().status_post(f + TAGS)
-        except Exception:
-            traceback.print_exc()
-        time.sleep(DELAY)
+        f = subprocess.run('fortune', text=True, stdout=subprocess.PIPE).rstrip()
+        if len(f) <= bot.max_body:
+            return f
 
-
-if __name__ == '__main__':
-    main()
+        print('...Fortune too long!', len(f))
