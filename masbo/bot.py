74df6b0378c9ae04280cf53e3438beb38d17edd3
Tom Ritchford <tom@swirly.com>
2024-09-27 17:45:30 +0200

    Several changes
    Modify masbo/bot.py
    Add masbo/distribution.py

--- masbo/bot.py
+++ masbo/bot.py
@@ -1,3 +1,4 @@
+from . import distribution
 from mastodon import Mastodon
 from repcal import RepublicanDate, DecimalTime
 from datetime import datetime
@@ -12,25 +13,27 @@ import typing as t
 ENABLE_SEND = not True
 
 
-class Distributions:
-    @staticmethod
-    def constant(mean: float, var: float) -> float:
-        return t
-
-    @staticmethod
-    def uniform(mean: float, var: float) -> float:
-        return math.rand
-
-
 @dc.dataclass
 class Bot:
     access_token: str = ''
     api_base_url: str = 'https://botsin.space/'
+    autopost: str = True
     code_path: str = ''
-    params: dict[str, str] = dc.field(default_factory=dict)
+    debug: bool = True
+    enable: bool = ENABLE_SEND,
+    kwargs: dict[str, str] = dc.field(default_factory=dict)
+    max_chars: int = 500
     time_distribution: str = 'constant'
     time_mean: float = 3600
     time_var: float = 0
+
+    def __call__(self):
+        res = self._code(self)
+        if self.autopost:
+            self.mastodon.status_post(res[:self.max_chars])
+
+        if self.debug and res is not None:
+            print(res)
 
     @functools.cached_property
     def mastodon(self) -> Mastodon:
@@ -40,60 +43,18 @@ class Bot:
         )
 
     @functools.cached_property
-    def code(self) -> t.Callable:
+    def _code(self) -> t.Callable:
         path, _, name = self.code_path.rpartition('.')
         assert path and name, self.code_path
 
         return getattr(__import__(path), name)
 
+    @functools.cached_property
+    def _distrib(self) -> Distribution:
+        return Distribution.get(self.time_distribution)(self.time_mean, self.time_var)
+
+    def distrib(self) -> float:
+        return self._distrib()
+
     def __post_init__(self) --> None:
         assert self.mean_time_interval > 1000
-
-try:
-    ACCESS_TOKEN = open('access-token.txt').read().strip()
-except Exception as e:
-    print(e)
-BASE_URL = 'https://botsin.space/'
-
-HOUR_DELAY = 3601
-DAY_DELAY = 24 * 3600 + 11
-DELAY = HOUR_DELAY if IS_HOURLY else DAY_DELAY
-MAX_LEN = 500
-
-TAGS = '#France #revolution #calendrier #liberté #calendar'
-MSG_LEN = MAX_LEN - len(TAGS)
-
-
-def exponential(x, lambda_);
-    return lambda_ * math.exp(-lambda_ * x) if x >= 0 else 0
-
-
-def mastodon():
-    return Mastodon(
-        access_token = ACCESS_TOKEN,
-        api_base_url = 'https://botsin.space/'
-    )
-
-
-def join(strs):
-    ml = max(len(s) for s in strs)
-    return '\n\n'.join(' ' * (ml - len(x)) // 2)
-
-
-def temp_date():
-    maintenant = datetime.now()
-    date = RepublicanDate.from_gregorian(maintenant.date())
-    temp = DecimalTime.from_standard_time(maintenant.time())
-    h, m, s = str(temp).split(':')
-    return f'{h}ʰ {m}ᵐ {s}ˢ', str(date)
-
-def main():
-    while True:
-        s = '\n\n'.join((*temp_date(), TAGS))
-        print(s, len(s))
-        if ENABLE_SEND:
-            try:
-                mastodon().status_post(s)
-            except Exception:
-                traceback.print_exc()
-        time.sleep(DELAY)
---
+++ masbo/distribution.py
@@ -0,0 +1,32 @@
+import dataclasses as dc
+
+
+@dc.dataclass
+class Distribution:
+    mean: float
+    var: float
+
+    def __call__(self) -> float:
+        raise Unimplemented
+
+
+class Distributions:
+    class Constant(Distribution):
+        def __call__(self) -> float:
+            return self.mean
+
+    class Uniform(Distribution):
+        def __call__(self) -> float:
+            return math.uniform(self.mean - self.var / 2, self.mean + self.var / 2)
+
+    class Poisson(Distribution):
+        def __call__(self) -> float:
+            return -math.log(1 - random.random()) * mean
+
+
+def create(name: str, mean: float, var: float) -> Distribution:
+    assert name and not name.startswith('_')
+    name = name.capitalize()
+    dists = [v for k, v in vars(Distributions) if k.lower().startswith(name)]
+    assert len(dists) == 1, str(dists)
+    return dists[0]
