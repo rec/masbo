69c1a825b1b217f4846233ad80246f7c7ceb448a
Tom Ritchford <tom@swirly.com>
2024-09-27 21:10:34 +0200

    Extract bot_collection.py from bot_runner.py

---
+++ masbo/bot_collection.py
@@ -0,0 +1,27 @@
+import heapq
+import json
+import time
+from . bot_runner import BotRunner
+
+
+class BotCollection:
+    def __init__(self, bots_file: str, tokens_file: str) -> None:
+        bots = json.load(open(bots_file))
+        tokens = json.load(open(tokens_file))
+
+        assert isinstance(bots, dict) and isinstance(tokens, dict)
+        assert sorted(bots) == sorted(tokens)
+
+        for name, desc in self.descs.items():
+            desc['access_token'] = tokens[name]
+
+        self.runners = {k: BotRunner.create(**v) for k, v in bots.items()}
+
+    def run(self) -> None:
+        heap = [(time.time(), str(r.desc), r) for r in self.runners]
+
+        while True:
+            t, key, runner = heap[0]
+            heapq.heapreplace(heap, (t + runner(), key, runner))
+            if (next_event := heap[0][0] - time.time()) > 0:
+                time.sleep(next_event)
--- masbo/bot_runner.py
+++ masbo/bot_runner.py
@@ -1,10 +1,6 @@
 from . bot_description import BotDescription
 from mastodon import Mastodon
-import heapq
-import json
-import time
 import traceback
-import typing as t
 
 
 class BotRunner:
@@ -36,24 +32,6 @@ class BotRunner:
 
         return self.desc.distrib()
 
-
-def run(bots: t.Sequence[BotRunner]):
-    heap = [(time.time(), str(b), b) for b in bots]
-
-    while True:
-        t, key, bot = heap[0]
-        heapq.heapreplace(heap, (t + bot(), key, bot))
-        if (next_event := heap[0][0] - time.time()) > 0:
-            time.sleep(next_event)
-
-
-def read(bots_file: str, tokens_file: str) -> dict[str, t.Any]:
-    bots = json.load(open(bots_file))
-    tokens = json.load(open(tokens_file))
-
-    assert isinstance(bots, dict) and isinstance(tokens, dict)
-    assert sorted(bots) == sorted(tokens)
-    for name, bot in bots.items():
-        bot.access_token = tokens[name]
-
-    return bots
+    @staticmethod
+    def create(**kwargs) -> 'BotRunner':
+        return BotRunner(BotDescription(**kwargs))
